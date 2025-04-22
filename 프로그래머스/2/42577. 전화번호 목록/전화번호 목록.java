a750930620ea402268c842aff4ad871ed94a1e1e
Yun Jisu <55443621+a121255@users.noreply.github.com>
2025-04-22 17:27:12 +0900

    [level 2] Title: 전화번호 목록, Time: 293.69 ms, Memory: 220 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+       Map<String, Integer> map = new HashMap<>();
+
+        Arrays.stream(phone_book).forEach(str -> map.put(str, 0));
+
+        for (String s : phone_book) {
+            for (int j = 0; j < s.length(); j++) {
+                String prefix = s.substring(0, j);
+                if (map.containsKey(prefix)) return false;
+            }
+        }
+
+        return true;
+    }
+}

d9994c47592014b69c2fe17efc2d26cd9fd96acf
Yun Jisu <55443621+a121255@users.noreply.github.com>
2025-05-25 13:15:26 +0900

    [Bronze V] Title: A+B - 8, Time: 68 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -6,17 +6,18 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        st = new StringTokenizer(br.readLine());
-        int cnt = Integer.parseInt(st.nextToken());
-        
+        StringTokenizer st;
+
+        int cnt = Integer.parseInt(br.readLine());
+
         for (int i = 1; i <= cnt; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             sb.append("Case #").append(i).append(": ").append(a).append(" + ").append(b).append(" = ").append(a+b).append("\n");
         }
+
         System.out.println(sb.toString());
     }
 }

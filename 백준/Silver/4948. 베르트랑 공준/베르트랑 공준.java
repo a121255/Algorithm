e1ae2b6dc0a504bda8d6587c87079f2e99ba2c2f
Yun Jisu <55443621+a121255@users.noreply.github.com>
2025-05-29 13:59:07 +0900

    [Silver II] Title: 베르트랑 공준, Time: 140 ms, Memory: 23748 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int s;
+        while((s = Integer.parseInt(br.readLine())) != 0) {
+            sb.append(isPrime(s)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static int isPrime(int num) {
+        int m = num * 2;
+        boolean[] isNotPrime = new boolean[m + 1];
+        isNotPrime[0] = true;
+        isNotPrime[1] = true;
+
+        for(int i = 2; i * i <= m; i++) {
+            if(!isNotPrime[i]) {
+                for(int j = i; j <= m; j+=i ){
+                    isNotPrime[j] = true;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for(int i = num + 1; i <= m; i++){
+            if(!isNotPrime[i]) cnt++;
+        }
+        return cnt;
+    }
+}

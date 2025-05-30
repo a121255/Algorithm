476b4923075b30cde38b1500197804f63bc5de53
Yun Jisu <55443621+a121255@users.noreply.github.com>
2025-05-30 15:07:01 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 60 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -4,15 +4,13 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
-
+          
         String[] arr = br.readLine().split(" ");
         int a = Integer.parseInt(arr[0]);
         int b = Integer.parseInt(arr[1]);
         
         System.out.println(gcd(a, b));
         System.out.println(lmc(a, b));
-
-
     }
     
     public static int gcd(int a, int b) {

eb92ab8f995d066e57e8339ab9f929768d768a93
Yun Jisu <55443621+a121255@users.noreply.github.com>
2025-05-31 18:49:59 +0900

    [Bronze I] Title: 소인수분해, Time: 96 ms, Memory: 11700 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int i = 2;
+        while(num != 1) {
+            if(num % i == 0){
+                sb.append(i).append("\n");
+                num /= i;
+            }else {
+                i++;
+            }
+        }
+        System.out.println(sb);
+    }
+}
